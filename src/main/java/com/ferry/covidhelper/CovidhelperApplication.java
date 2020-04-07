34a817e7660dc4be3e3247dd3b76639c38cb1c85
yagohenrique <yago.ht@gmail.com>
2020-04-06 22:49:58 -0300

    Init project

---
+++ src/main/java/com/ferry/covidhelper/CovidhelperApplication.java
@@ -0,0 +1,13 @@
+package com.ferry.covidhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferry/covidhelper/CovidhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferry.covidhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
