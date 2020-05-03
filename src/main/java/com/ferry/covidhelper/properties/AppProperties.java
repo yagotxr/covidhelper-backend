82c1d6e4243d4766e282c8dcc2f02867587b4964
yagohenrique <yago.ht@gmail.com>
2020-05-03 17:24:09 -0300

    Added configuration properties to oAuth2

--- src/main/java/com/ferry/covidhelper/properties/AppProperties.java
+++ src/main/java/com/ferry/covidhelper/properties/AppProperties.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "app")
 @Getter
 public class AppProperties {
+
     private final Auth auth = new Auth();
     private final OAuth2 oauth2 = new OAuth2();
 
