f1c85ae92a07f7c9fa15f7680de7fa2e5d772a0e
yagohenrique <yagotxr@bitbucket.org>
2020-05-25 00:29:54 -0300

    Refactored.

--- src/main/java/com/ferry/covidhelper/security/user/CustomOAuth2UserService.java
+++ src/main/java/com/ferry/covidhelper/security/user/CustomOAuth2UserService.java
@@ -3,21 +3,18 @@ package com.ferry.covidhelper.security.user;
 import com.ferry.covidhelper.domains.User;
 import com.ferry.covidhelper.exceptions.OAuth2AuthenticationProcessingException;
 import com.ferry.covidhelper.repositories.UserRepository;
-import com.ferry.covidhelper.security.user.OAuth2UserInfo;
-import com.ferry.covidhelper.security.user.OAuth2UserInfoFactory;
-import com.ferry.covidhelper.security.user.UserPrincipal;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
 
+import static com.ferry.covidhelper.security.user.OAuth2UserInfoFactory.getOAuth2UserInfo;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 @Service
@@ -32,22 +29,23 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         try {
             return processOAuth2User(oAuth2UserRequest, oAuth2User);
-        } catch (AuthenticationException ex) {
-            throw ex;
-        } catch (Exception ex) {
-            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
-            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+        } catch (Exception exception) {
+            throw new InternalAuthenticationServiceException(exception.getMessage(), exception.getCause());
         }
     }
 
     private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
-        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
+        String registrationId = oAuth2UserRequest.getClientRegistration().getRegistrationId();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        OAuth2UserInfo oAuth2UserInfo = getOAuth2UserInfo(registrationId, attributes);
         if(isEmpty(oAuth2UserInfo.getEmail())) {
             throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
         }
 
-        Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
         User user;
+        Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
+
         if(userOptional.isPresent()) {
             user = userOptional.get();
             user = updateExistingUser(user, oAuth2UserInfo);
